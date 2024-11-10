ed501cfd6791aafc5b957c637a0b2b45535628f9
dkpanigrahi <dkpanigrahi2001@gmail.com>
2024-11-10 14:02:05 +0530

    Created Home

--- src/app/component/user-home/user-home.component.ts
+++ src/app/component/user-home/user-home.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { SearchBusComponent } from "../search-bus/search-bus.component";
+import { FooterComponent } from "../footer/footer.component";
+import { RouteConfigLoadEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [SearchBusComponent],
+  imports: [FooterComponent],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
 export class UserHomeComponent {
+
+  constructor(private router:Router){}
+
+navigateToTicketPage() {
+  this.router.navigate(['/bus'])
+}
 
 }
